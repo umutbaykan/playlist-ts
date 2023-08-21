629ee71db1950a5082236574f35ee1d786401a87
umut <umut_baykan@hotmail.com>
2023-08-21 16:31:50 +0100

    initializing playlist object

--- src/playlist.ts
+++ src/playlist.ts
@@ -1,3 +1,15 @@
-export const add = (x: number, y:number): number => {
-  return x+y;
+import Song from "./song"
+
+class Playlist {
+  songs: Song[];
+
+  constructor(songs: Song[]) {
+    this.songs = songs
+  }
+
+  getSongs = ():Song[] => {
+    return this.songs;
+  }
 }
+
+export default Playlist;
--- test/song.test.ts
+++ test/song.test.ts
@@ -1,7 +1,7 @@
 import Song from '../src/song';
 
-describe("test add function", () => {
-  it("should initialize with the constructor", () => {
+describe("song class", () => {
+  test("should initialize with the constructor", () => {
     const song:Song = new Song("title", "artist", 241);
     expect(song.getTitle()).toEqual("title");
     expect(song.getLength()).toEqual(241);
